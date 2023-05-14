a2ec78123a0abab03eec1b7d3f80ff4c39ed8382
exe <98409464+exe@users.noreply.github.com>
2023-05-14 23:49:22 +0900

    수정사항3

--- Assets/Script/Scenes/GameScene.cs
+++ Assets/Script/Scenes/GameScene.cs
@@ -4,23 +4,24 @@ using UnityEngine;
 
 public class GameScene : BaseScene
 {
-    class Test
-    {
-        public int id = 0;
-    }
+    //class Test
+    //{
+    //    public int id = 0;
+    //}
 
-    class CoroutineTest : IEnumerable
-    {
-        public IEnumerator GetEnumerator()
-        {
-            // foreach가 이걸 호출로도 보는구나 신기하네@@@@@@@@@@@@ 산하의 함수도 호출해주는 기능이있다는것
-            yield return new Test() { id = 1 };
-            yield return new Test() { id = 2 };
-            yield return new Test() { id = 3 };
-            yield return new Test() { id = 4 };
-        }
-    }
+    //class CoroutineTest : IEnumerable
+    //{
+    //    public IEnumerator GetEnumerator()
+    //    {
+    //        // foreach가 이걸 호출로도 보는구나 신기하네@@@@@@@@@@@@ 산하의 함수도 호출해주는 기능이있다는것
+    //        yield return new Test() { id = 1 };
+    //        yield return new Test() { id = 2 };
+    //        yield return new Test() { id = 3 };
+    //        yield return new Test() { id = 4 };
+    //    }
+    //}
 
+    //Coroutine co;
     protected override void Init()
     {
         base.Init();
@@ -32,14 +33,35 @@ public class GameScene : BaseScene
         //for (int i = 0; i < 5; i++)
         //    Managers.Resource.Instantiate("unitychan");
 
-        CoroutineTest test = new CoroutineTest();
-        // var에 마우스대면 무슨타입인지 알수있음@@
-        foreach (var t in test)
-        {
-            Test e = t as Test;
-            Debug.Log(e.id);
-        }
+        //CoroutineTest test = new CoroutineTest();
+        //// var에 마우스대면 무슨타입인지 알수있음@@
+        //foreach (var t in test)
+        //{
+        //    Test e = t as Test;
+        //    Debug.Log(e.id);
+        //}
+        //co = StartCoroutine("ExplodeAfterSeconds", 4.0f);
+        //StartCoroutine("CoStopExplode", 2.0f);
     }
+    //IEnumerator CoStopExplode(float seconds)
+    //{
+    //    Debug.Log("Stop Enter");
+    //    yield return new WaitForSeconds(seconds);
+    //    Debug.Log("Stop Excute!!!");
+    //    if (co != null)
+    //    {
+    //        StopCoroutine(co);
+    //        co = null;
+    //    }
+    //}
+
+    //IEnumerator ExplodeAfterSeconds(float seconds)
+    //{
+    //    Debug.Log("Explode Enter");
+    //    yield return new WaitForSeconds(seconds);
+    //    Debug.Log("Explode Excute!!!");
+    //    co = null;
+    //}
     public override void Clear()
     {
     }
